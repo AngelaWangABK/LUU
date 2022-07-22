fe19d482dbabfe80715ac48033ed68f433635d24
AngelaWangABK <angela.wang2@activision.com>
2022-07-21 17:09:52 -0700

    added input validation and ch4 array

--- LUUChallenges/Ch3/chapter3.cpp
+++ LUUChallenges/Ch3/chapter3.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using std::cout;
 using std::cin;
+using std::getline;
+using std::string;
 using std::vector;
 
 //challenges for chapter 3
@@ -10,7 +13,16 @@ using std::vector;
 void leapYear() {
 	int year = 0;
 	cout << "Please enter a year: ";
-	cin >> year;
+	//input validation
+	while (cin) {
+		cin >> year;
+		if (year < 0) {
+			cin.clear();
+		} else {
+			break;
+		}
+	}
+	//checking for leap year
 	if (year % 400 == 0) {
 		cout << "Leap year!";
 	} else if (year % 4 == 0 && year % 100 != 0) {
@@ -18,7 +30,6 @@ void leapYear() {
 	} else {
 		cout << "Not a leap year!";
 	}
-
 }
 
 void allFactors() {
@@ -41,7 +52,54 @@ void allFactors() {
 	}
 }
 
+//ch 4 - finds max int from array
+void maxArray() {
+	int i = 0;	
+	vector<int> v;
+	cout << "Please enter an array of integers: \n";
+	while (cin) {
+		cin >> i;
+		//input validation
+		if (cin.fail()) {
+			cout << "Invalid input. Please try again.\n";
+			cin.clear();
+			cin.ignore(10000, '\n');
+		} else {
+			v.push_back(i);
+			break;
+		}
+	}
+	
+	int max = 0;
+	for (int i : v) {
+		if (max < i) {
+			max = i;
+		}
+	}
+	cout << max;
+}
+
 int main() {
-	leapYear();
-	allFactors();
+
+	int x = 0;
+
+	cout << "Select function to test: \n" <<
+		"1. Leap year\n" <<
+		"2. Factors\n" <<
+		"3. Maximum number in array\n";
+	
+	cin >> x;
+	if (cin.fail() || x < 1 || x > 3) {
+		cout << "Invalid input. Please enter 1, 2, or 3. \n";
+	}
+	
+	switch (x) {
+	case 1: leapYear();
+		break;
+	case 2: allFactors();
+		break;
+	case 3: maxArray();
+		break;
+	}
+	
 }
