af2ebebd895da424db7781b8e165d44974f9ed57
AngelaWangABK <angela.wang2@activision.com>
2022-08-14 22:43:33 -0700

    adding chat application

---
+++ LUUChallenges/Week5Ch2/ChatManager.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <memory>
+#include <set>
+#include <string>
+
+#include "ChatMessagePubSub.h"
+#include "ChatMessage.h"
+
+namespace chat
+{
+	class Manager : public IMessageSubscriber
+	{
+	public:
+		Manager(Host* aHostPtr);
+		~Manager();
+
+		bool Run();
+		void OnMessageReceived(const Message& aMessage) override;
+
+	protected:
+		Host* mHostPtr{ nullptr };          /* A wrapped ENet connection                               */
+		std::string       mUsername{ "" };              /* The chat user's name                                    */
+		std::set<Message> mMessages{ };                 /* The queue of messages                                   */
+
+		std::string       mUserMessage{ "" };           /* Buffered user message                                   */
+		std::atomic<bool> mReadyToSend{ false };        /* Thread-safe flag for notifying message is ready to send */
+		std::atomic<bool> mFinished{ false };           /* Thread-safe flag for ending                             */
+
+		void tListenForMessages();
+		void tListenForUserInput();
+		void tSendMessageWhenReady();
+
+		void SendMessagePacket();
+	};
+}
---
+++ LUUChallenges/Week5Ch2/Utilities.h
@@ -0,0 +1,84 @@
+#pragma once
+#include <chrono>
+#include <iostream>
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace util
+{
+    // Check if aString is a number.
+    static const bool isNumber(const std::string& aString)
+    {
+        return !aString.empty() &&
+            aString.find_first_not_of("0123456789.") == std::string::npos;
+    }
+
+    // Check if aString is an integer.
+    static const bool isInteger(const std::string& aString)
+    {
+        return !aString.empty() &&
+            aString.find_first_not_of("0123456789") == std::string::npos;
+    }
+
+    // Print the contents of a vector separated by a space to an output stream.
+    template<typename T>
+    static void printVector(std::ostream& aOut, const std::vector<T>& aVec)
+    {
+        for (const auto& t : aVec)
+        {
+            aOut << t << " ";
+        }
+    }
+
+    // Get a string input from the user
+    static std::string GetUserInput(const std::string& aPrompt)
+    {
+        std::cout << aPrompt;
+
+        std::string input;
+        std::getline(std::cin, input);
+
+        return input;
+    }
+
+    // Get a number from the user on standard input.
+    // Returns empty optional if invalid number supplied.
+    template <typename T>
+    static std::optional<T> getNumberFromUser()
+    {
+        std::string userOption;
+        std::getline(std::cin, userOption);
+
+        std::optional<T> result;
+        if (isInteger(userOption))
+        {
+            result = std::stoi(userOption);
+        }
+        return result;
+    }
+
+    static std::optional<double> getDecimalNumberFromUser()
+    {
+        std::string userOption;
+        std::getline(std::cin, userOption);
+
+        std::optional<double> result;
+        if (isNumber(userOption))
+        {
+            result = std::stod(userOption);
+        }
+        return result;
+    }
+
+    static uint32_t getNanos()
+    {
+        using namespace std::chrono;
+        return static_cast<uint32_t>(
+            duration_cast<nanoseconds>(
+                system_clock::now().time_since_epoch()
+                ).count()
+            );
+    }
+
+}
